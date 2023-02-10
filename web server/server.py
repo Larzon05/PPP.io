404a48e94f64e368d6fe73f498cbd68a096744c8
Zoboki Csanád <csanad.zoboki@gmail.com>
2023-02-10 08:40:18 +0100

    CSV update

--- web server/server.py
+++ web server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 app = Flask(__name__)
 
 
@@ -13,8 +14,31 @@ def my_homepage2(page_name):
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method =='POST':
-        data = request.form.to_dict()
-        print(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            #write_to_file(data)
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            'Did not save to database'
     else:
         return 'something went wrong'
+
+def write_to_file(data):
+    with open('database.txt',mode='a', encoding='utf-8') as db:
+        name = data['name']
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        
+        text_file = db.write(f'\nName: {name}, email: {email}, subject: {subject}, message: {message}')
+        
+def write_to_csv(data):
+    with open('database.csv',mode='a', encoding='utf-8',newline='') as db_csv:
+        name = data['name']
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        
+        csv_file = csv.writer(db_csv,'excel')
+        csv_file.writerow([name,email,subject,message])
